86df688def4fab25f80772321452a2ca22021d8e
xavishacodes <65103675+xavishacodes@users.noreply.github.com>
2024-03-01 19:26:42 +0530

    Update kill.bat

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,16 +1,11 @@
 @echo off
 
 echo The following command terminates the "npm start" process using its PID
-echo (written to ".pidfile"), all of which were conducted when "deliver.sh"
+echo (written to ".pidfile"), all of which were conducted when "deliver.bat"
 echo was executed.
-echo.
 
-setlocal enabledelayedexpansion
-for /f %%a in (.pidfile) do (
-    taskkill /PID %%a /F > nul 2>&1
-    if errorlevel 1 (
-        echo Failed to kill process with PID %%a
-    ) else (
-        echo Process with PID %%a killed successfully
-    )
-)
+rem Read the PID from .pidfile and kill the process
+set /p PID=<.pidfile
+taskkill /PID %PID% /F
+
+echo Process with PID %PID% killed successfully
